187e84108f2e948d350eb19381811b8d4538f911
Amanda McCumber <Serenity333@users.noreply.github.com>
2016-05-06 17:34:03 -0400

    Update circle.py

--- circle.py
+++ circle.py
@@ -1,9 +1,12 @@
 #Draws a circle via a 50 sided polygon using a Turtle; WARNING: Does NOT include Turtle set up. 
-#Requires polygon script
 
 import math *
 
-import polygon
+def polygon(t, n, length):
+    angle = 360 / n
+    for i in range(n):
+        t.fd(length)
+        t.lt(angle)
 
 def circle(t, r):
     circumference = 2 * math.pi * r
